d823c6fea70f1351f0c2c176949c6c598b54b514
cdunham927 <cdunham927@gmail.com>
2019-03-04 17:57:44 -0800

    Started enemy controller

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyController : MonoBehaviour
+{
+    [Header("Main stats")]
+    [SerializeField] float hp;
+    [SerializeField] float atk;
+    [SerializeField] float def;
+    [SerializeField] float evade;
+    [Space]
+    [Header("Stats for spawn rate")]
+    [SerializeField] float spawnRate;
+    [SerializeField] float buddySpawnRate;
+
+    public virtual void MainAttack() { }
+    public virtual void SecondaryAttack() { }
+}
---
+++ Assets/Scripts/GoblinController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinController : EnemyController
+{
+    public override void MainAttack()
+    {
+
+    }
+
+    public override void SecondaryAttack()
+    {
+
+    }
+}
