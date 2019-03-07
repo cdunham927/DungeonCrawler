5578840cd31b5e5d3fa11399423f179c550322ec
Jesse Mendoza <33880932+jessmend815@users.noreply.github.com>
2019-03-06 22:49:17 -0800

    Made Turn Base With 1 Enemy

---
+++ Assets/Scripts/BattleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleScript : PlayerController
+{
+    public Button attackButton;
+
+    void Start()
+    {
+        Button btn = attackButton.GetComponent<Button>();
+        btn.onClick.AddListener(AttackOnClick);
+    }
+    void AttackOnClick()
+    {
+        //Output this to console when Button is clicked
+        Debug.Log("You have dealt damage!");
+    }
+}
--- Assets/Scripts/GoblinController.cs
+++ Assets/Scripts/GoblinController.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoblinController : EnemyController
-{
-    public override void MainAttack()
+{   
+    BattleController controller;
+    public Image health_bar;
+    public Text health;
+    
+    void Awake() 
     {
-
+        controller = GameObject.Find("BattleController").GetComponent<BattleController>();
     }
 
-    public override void SecondaryAttack()
+    public override void MainAttack()
     {
+        controller.player.TakeDamage(atk);
+    }
+    
 
+    //public override void SecondaryAttack()
+    //{
+
+    //}
+
+    public override void TakeDamage(float dmg)
+    {
+        hp -= dmg;
+    }
+
+    public void DeadEnemy()
+    {
+        if (hp == 0)
+        {
+            Destroy (gameObject);
+        }
+    }
+    private void Update()
+    {
+        //Health
+        health_bar.fillAmount = hp / maxHP;
+        health.text = "Goblin HP: " + hp;
+        DeadEnemy();
     }
 }
